e0a2c42bea5a041b3d932a8ebcda5b2654195ffc
JuhiLasod <juhilasod29@gmail.com>
2025-05-19 15:47:21 +0530

    cors added

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import authRoutes from "./Routes/authRoutes.js";
 dotenv.config();
 const app=express();
 app.use(cors({
-    origin: '', 
+    origin: 'https://gps-tracking-x6qv.onrender.com', 
     credentials: true, 
   }));
 app.use(express.json());
